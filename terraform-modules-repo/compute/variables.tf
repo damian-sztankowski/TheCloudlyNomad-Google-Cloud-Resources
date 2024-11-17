ff015995bf811c14e71acedc684457283f851a74
Damian <damian.sztankowski@gmail.com>
2024-11-17 13:21:05 +0100

    Create variables.tf

---
+++ terraform-modules-repo/compute/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type        = string
+  description = "The Google Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The region for deployment"
+}
+
+variable "zone" {
+  type        = string
+  description = "The zone for deployment"
+}
