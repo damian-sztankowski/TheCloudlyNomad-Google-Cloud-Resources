a0b534cacc3c8915dfdcbe0db2b4906d320fdde2
Damian <damian.sztankowski@gmail.com>
2024-11-17 13:20:43 +0100

    Create main.tf

---
+++ terraform-modules-repo/compute/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "web-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
